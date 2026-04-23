fa81b0b34718400e1b3c4e8cbe6bc7f4d49f83ee
Pablo <147423709+100509590@users.noreply.github.com>
2026-04-23 13:44:36 +0200

    Add files via upload

---
+++ src/main/java/es/uc3m/tienda/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package es.uc3m.tienda.repositories;
+
+import es.uc3m.tienda.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Spring Data genera el SQL automáticamente a partir del nombre del método
+    Optional<User> findByEmail(String email);
+}
+
